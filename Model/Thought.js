d4533ff36df09e854e1a543f42e4af838a335982
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-09-20 21:29:13 -0500

    Models completed

--- Model/Thought.js
+++ Model/Thought.js
@@ -1,42 +1,49 @@
-Reaction Schema:(Though model sub-document schema)
+const { Schema } = require("mongoose");
+var validator = require("validator");
+//Though model sub-document schema)
+const ReactionSchema = new Schema({
+  replyId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now, //Use a getter method to format the timestamp on query
+  },
+});
 
-reactionId
-Use Mongoose's ObjectId data type
-Default value is set to a new ObjectId
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now, //Use a getter method to format the timestamp on query
+  },
+  username: {
+    type: String,
+    required: true,
+  },
 
-reactionBody
-String
-Required
-280 character maximum
+  reactions: [ReactionSchema], //Array of nested documents created with the reactionSchema
+});
 
-username
-String
-Required
+//*virtual reactionCount that retrieves the length of the thought's reactions array field on query.
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
-createdAt
-Date
-Set default value to the current timestamp
-Use a getter method to format the timestamp on query
+const Thought = model("thought", ThoughtSchema);
 
-
-Thought Schema:
-
-thoughtText
-String
-Required
-Must be between 1 and 280 characters
-
-createdAt
-Date
-Set default value to the current timestamp
-Use a getter method to format the timestamp on query
-
-username (The user that created this thought)
-String
-Required
-
-reactions (These are like replies)
-Array of nested documents created with the reactionSchema
-
-
-*virtual reactionCount that retrieves the length of the thought's reactions array field on query.
+module.exports = Thought;
--- Model/User.js
+++ Model/User.js
@@ -1,22 +1,41 @@
-username;
-String;
-Unique;
-Required;
-Trimmed;
+const { Schema, model } = require("mongoose");
 
+const { default: isEmail } = require("validator/lib/isEmail");
 
-email
-String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, "invaid email"],
+    //or validate: [validateEmail, 'please enter a valid email address']
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "thoughts",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+});
+//friends:  Array of _id values referencing the User model (self-reference)
 
+//**virtual friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length, 0;
+});
 
-thoughts
-Array of _id values referencing the Thought model
+const User = model("user", UserSchema);
 
-
-friends
-Array of _id values referencing the User model (self-reference)
-
-**virtual friendCount that retrieves the length of the user's friends array field on query.
+module.exports = User;
